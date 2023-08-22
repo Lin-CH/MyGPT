0b445f14f3a61417ef8580368d8a8cc121531e5e
LCH <1269915479@qq.com>
2023-08-22 11:40:25 +0800

    mygpt

---
+++ src/store/modules/user/helper.ts
@@ -0,0 +1,32 @@
+import { ss } from '@/utils/storage'
+
+const LOCAL_NAME = 'userStorage'
+
+export interface UserInfo {
+  avatar: string
+  name: string
+  description: string
+}
+
+export interface UserState {
+  userInfo: UserInfo
+}
+
+export function defaultSetting(): UserState {
+  return {
+    userInfo: {
+      avatar: 'https://img2.baidu.com/it/u=1703504609,3321138209&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=581',
+      name: '小辉辉',
+      description: 'v+GRAYLCH',
+    },
+  }
+}
+
+export function getLocalState(): UserState {
+  const localSetting: UserState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalState(setting: UserState): void {
+  ss.set(LOCAL_NAME, setting)
+}
